d62c76f8b7d695cba3e16490c31356a6ef446cc8
olanguyenvan <olanguyenvan@gmail.com>
2017-12-11 22:45:15 +0100

    day 3

---
+++ day3/3.py
@@ -0,0 +1,19 @@
+import math
+
+
+def find_steps_count_from(n):
+
+    innser_square_length = math.sqrt(n) // 1
+    if innser_square_length % 2 == 0:
+        innser_square_length += - 1
+
+    diff = n - innser_square_length * innser_square_length
+    if diff == 0:
+        return innser_square_length - 1
+
+    number_of_sides_to_pass = (diff / (innser_square_length + 1)) // 1
+    steps_in_side_to_pass = diff - number_of_sides_to_pass * (innser_square_length + 1)
+    return innser_square_length // 2 + 1 + math.fabs(innser_square_length // 2 + 1 - steps_in_side_to_pass)
+
+if __name__ == '__main__':
+    print(find_steps_count_from(312051))
